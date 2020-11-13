d5d31d051b1e9a2e780a217e80e6ca195df1176f
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-11-13 13:47:25 -0600

    Initial commit.

---
+++ src/main/java/org/acme/dynamodb/AwsLambdaHandler.java
@@ -0,0 +1,36 @@
+package org.acme.dynamodb;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.List;
+import java.util.Map;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+//@Named("amazonDynamodbQuickstart")
+//public class AwsLambdaHandler implements RequestHandler<Map<String, String>, String> {
+//	@Override
+//	public String handleRequest(Map<String, String> event, Context context) {
+//		return "Hello amazon-dynamodb-quickstart";
+//	}
+//}
+
+@Named("amazonDynamodbQuickstart")
+@ApplicationScoped
+public class AwsLambdaHandler implements RequestHandler<Map<String, String>, List<Fruit>> {
+
+	@Inject
+	FruitSyncService service;
+
+	@Override
+	public List<Fruit> handleRequest(Map<String, String> event, Context context) {
+//		List<Fruit> fruit = service.findAll();
+//		return fruit.get(0).getName();
+//		return fruit.size();
+//		return service.findAll().size();
+//		return 25;
+		return service.findAll();
+	}
+}
